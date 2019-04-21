e1d41486cac81fe4c34484838eafa91e057946e9
Marcin Wiśnios <marcinw1998@gmail.com>
2019-04-21 18:05:48 +0200

    Added VBO, VAO, shader parser and compiler

--- SpaceInvaders/src/Application.cpp
+++ SpaceInvaders/src/Application.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
 
 
 void processInput(GLFWwindow* window)
@@ -16,6 +19,46 @@ void framebuffer_size_callback(GLFWwindow * window, int width, int height)
 	std::cout << "Changed window size to: " << width << "x" << height << std::endl;
 	glViewport(0, 0, width, height);
 }
+
+
+struct ShaderSource
+{
+	std::string VertexShader;
+	std::string FragmentShader;
+};
+
+static ShaderSource ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else
+		{
+			ss[(int)type] << line << "\n";
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
+}
+
+
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -49,6 +92,65 @@ int main()
 		return -1;
 	}
 
+	// ---------------------------------- SHADER COMPILATION ----------------------------------
+
+	// Load shader source
+	ShaderSource ss = ParseShader("res/shaders/Basic.shader");
+
+	// Compiling vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	const char* src = (ss.VertexShader).c_str();
+	glShaderSource(vertexShader, 1, &src, NULL);
+	glCompileShader(vertexShader);
+
+	// Check for errors
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "[Vertex Shader] Compilation failed.\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	src = (ss.FragmentShader).c_str();
+	glShaderSource(fragmentShader, 1, &src, NULL);
+	glCompileShader(fragmentShader);
+
+	// Check for errors
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "[Fragment Shader] Compilation failed.\n" << infoLog << std::endl;
+	}
+
+	// Shader program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Check for errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "[Shader Program] Linking shader program failed.\n";
+	}
+
+	glUseProgram(shaderProgram);
+
+	// Removing shaders
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	// ---------------------------------- DATA FOR DRAWING ----------------------------------
 
 	float vertices[] = {
@@ -57,8 +159,24 @@ int main()
 		 0.0f,  0.5f
 	};
 
-	unsigned int VertexBufferObject;
+	unsigned int VertexBufferObject, VertexArrayObejct;
+
+	// Generating vertex array and buffer
+	glGenVertexArrays(1, &VertexArrayObejct);
 	glGenBuffers(1, &VertexBufferObject);
+
+	// Binding vertex array and buffer
+	glBindVertexArray(VertexArrayObejct);
+	glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
+
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), NULL);
+	glEnableVertexAttribArray(0);
+
+	// Unbinding
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
@@ -68,6 +186,9 @@ int main()
 		// render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glBindVertexArray(VertexArrayObejct);
+		glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
